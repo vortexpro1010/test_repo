4d9d0152b02c117120b8462e78a30190b6751715
vortexpro1010 <cmihai19711@gmail.com>
2022-04-03 21:03:01 +0300

    lol

---
+++ test.py
@@ -0,0 +1,11 @@
+print("Incredible")
+
+print("Hello my friend, what's up brohhh")
+
+for i in range(0, 10):
+    print("meen")
+
+list = ["lol", "wtf", "yeye"]
+
+for i in range(0, 10):
+    print("Hello my friend")
